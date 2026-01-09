49c1ea007fd379960f054f1a7fc9ee413d3bc2a1
TanganIkan <iputupermanaadiputra@gmail.com>
2026-01-09 12:56:03 +0800

    add crud

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,14 +3,42 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ArticleController extends Controller
 {
     public function index()
     {
-        $articles = Article::with('user', 'category')->latest()->get();
+        if (auth()->user()->role === 'admin') {
+            $articles = Article::with('category')->latest()->get();
+        } else {
+            $articles = Article::where('user_id', auth()->id())->with('category')->latest()->get();
+        }
         return view('dashboard', compact('articles'));
     }
+
+    public function create()
+    {
+        $categories = Category::all();
+        return view('articles.create', compact('categories'));
+    }
+
+    public function store(Request $request)
+    {
+
+        Article::create([
+            'title' => request('title'),
+            'slug' => Str::slug(request('title')),
+            'content' => request('content'),
+            'category_id' => request('category_id'),
+            'user_id' => auth()->id(),
+        ]);
+
+        return redirect()->route('articles.index')->with('success', 'Article created successfully.');
+    }
+
 
 }
